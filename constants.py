40a93efd04606d8986bc7514973d88bffc2ac92e
Michael Richardson <mrcsrichie@gmail.com>
2018-04-29 15:26:32 +0000

    Added song data to the contants.py.

--- constants.py
+++ constants.py
@@ -1,4 +1,5 @@
 import course
+import song
 
 COURSES = [
     course.Course(period=1,
@@ -31,4 +32,47 @@ COURSES = [
                   teacher_name='Mr. Poulsen',
                   resource_name='Wikipedia',
                   resource_url='https://en.wikipedia.org/wiki/Biology'),
+]
+
+TOP_TEN_SONGS = [
+    song.Song(
+        title='Call Out My Name',
+        artist_name='The Weeknd',
+        youtube_url='https://youtu.be/M4ZoCHID9GI'),
+    song.Song(
+        title='IDGAF',
+        artist_name='Dua Lipa',
+        youtube_url='https://youtu.be/Mgfe5tIwOj0'),
+    song.Song(
+        title='Perfect',
+        artist_name='Ed Sheeran',
+        youtube_url='https://youtu.be/2Vv-BfVoq4g'),
+    song.Song(
+        title='Havana',
+        artist_name='Camila Camello',
+        youtube_url='https://youtu.be/pz95u3UVpaM'),
+    song.Song(
+        title='Psycho',
+        artist_name='Post Malone',
+        youtube_url='https://youtu.be/au2n7VVGv_c'),
+    song.Song(
+        title='FRIENDS',
+        artist_name='Marshmello & Anne-Marie',
+        youtube_url='https://youtu.be/jzD_yyEcp0M'),
+    song.Song(
+        title='Colors',
+        artist_name='Jason Derulo',
+        youtube_url='https://youtu.be/jzD_yyEcp0M'),
+    song.Song(
+        title='Let Me',
+        artist_name='ZAYN',
+        youtube_url='https://youtu.be/J-dv_DcDD_A'),
+    song.Song(
+        title='New Rules',
+        artist_name='Dua Lipa',
+        youtube_url='https://youtu.be/k2qgadSvNyU'),
+    song.Song(
+        title="God's Plan",
+        artist_name='Drake',
+        youtube_url='https://youtu.be/xpVfcZ0ZcFM'),
 ]
