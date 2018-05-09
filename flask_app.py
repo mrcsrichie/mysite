6866e5cbaa1e9fff267545255146a8378b34c16b
Michael Richardson <mrcsrichie@gmail.com>
2018-05-09 02:03:32 +0000

    Add class for song table.

--- flask_app.py
+++ flask_app.py
@@ -17,6 +17,12 @@ class Course(db.Model):
     resource_name = db.Column(db.String(80))
     resource_url = db.Column(db.String(300))
 
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    artist_name = db.Column(db.String(80))
+    youtube_url = db.Column(db.String(300))
+
 @app.route('/')
 def homepage():
     return render_template('index.html')
@@ -40,3 +46,6 @@ def register():
 def top_ten_songs():
     return render_template('top_ten_songs.html',
                            songs=constants.TOP_TEN_SONGS)
+
+if __name__ == '__main__':
+  db.create_all()
