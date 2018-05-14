d7aff620b768c9ad5b5b716349c3e92434b9cf59
Michael Richardson <mrcsrichie@gmail.com>
2018-05-14 00:49:05 +0000

    Add ability to post for logged in users.

--- flask_app.py
+++ flask_app.py
@@ -31,6 +31,7 @@ def create_navbar():
     home_view = View('Home', 'homepage')
     login_view = View('Login', 'login')
     logout_view = View('Logout', 'logout')
+    posts_view = View('Posts', 'posts')
     register_view = View('Register', 'register')
     about_me_view = View('About Me', 'about_me')
     class_schedule_view = View('Class Schedule', 'class_schedule')
@@ -40,10 +41,9 @@ def create_navbar():
                              class_schedule_view,
                              top_ten_songs_view)
     if current_user.is_authenticated:
-        return Navbar('MySite', home_view, misc_subgroup, logout_view)
+        return Navbar('MySite', home_view, posts_view, misc_subgroup, logout_view)
     else:
         return Navbar('MySite', home_view, misc_subgroup, login_view, register_view)
-
 class Course(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     period = db.Column(db.Integer)
@@ -95,13 +95,18 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Sign in')
 
+class PostForm(FlaskForm):
+    message = StringField('Message', validators=[InputRequired(), Length(max=280)])
+    submit = SubmitField('Post')
+
 @login.user_loader
 def load_user(user_id):
     return User.query.filter_by(id=int(user_id)).first()
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    recent_posts = Post.query.order_by(Post.timestamp.desc()).limit(20).all()
+    return render_template('index.html', posts=recent_posts)
 
 @app.route('/aboutme')
 @app.route('/about_me')
@@ -132,6 +137,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('homepage'))
+
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+    form = PostForm()
+    posts = Post.query.filter_by(user_id=current_user.id).all()
+    if form.validate_on_submit():
+        new_post = Post(user_id=current_user.id, body=form.message.data)
+        db.session.add(new_post)
+        db.session.commit()
+        posts.append(new_post)
+    return render_template('posts.html', form=form, posts=posts)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
